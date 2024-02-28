ba9663530e75cf9bb36a3ca6e2a07cabeefe6289
Daxter <daxterman62@gmail.com>
2024-02-28 09:44:57 +0100

    Commit initial

---
+++ Back/app.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const fs = require("fs");
+var request = require('request');
+var express = require('express');
+var app = express();
+
+var client_id = process.env.ClientID;
+var client_secret = process.env.SecretID;
+var token = '';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}));
+
+const authOptions = {
+  method: 'POST',
+  headers: {
+    'Authorization': 'Basic ' + btoa(`${client_id}:${client_secret}`),
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: 'grant_type=client_credentials',
+};
+  
+
+
+async function playlist(token,id)
+{
+    const response = await fetch('https://api.spotify.com/v1/playlists/'+id+"/tracks", {
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
+  });
+
+  const data = await response.json();
+  console.log(data);
+  fs.writeFile('reponse.json', JSON.stringify(data, null, 2), (err) => {
+    if (err) throw err;
+    console.log('La réponse a été écrite dans le fichier reponse.json');
+  });
+}
+
+fetch("https://accounts.spotify.com/api/token",authOptions).then(resp => resp.json()).then(json => console.log(json))
+
+
+const temp = JSON.parse(fs.readFileSync("./reponse.json"));
+
+var tab = [];
+
+temp.items.forEach(element => {
+  let artists = [];
+  element.track.artists.forEach(ele => {artists.push(ele.name);});
+
+  tab.push(
+    {
+      "name": element.track.name,
+      "img" : element.track.album.images[0].url,
+      "id": element.track.id,
+      "artist" : artists
+    }
+  );
+});
+
+
+
+app.get("/",function(req,res){
+  const shuffle = tab.sort((a, b) => 0.5 - Math.random());
+  res.json(shuffle);
+})
+
+
+app.listen(8080);
+console.log("App listening on port 8080...");
---
+++ Front/js/Lecteur.js
@@ -0,0 +1,92 @@
+var tab;
+var i = 0;
+var pause = true;
+var t_restant = 29;
+var f = null;
+
+
+document.addEventListener('DOMContentLoaded',async function() {
+  // Votre fonction à exécuter au chargement de la page
+  res =  await fetch("http://localhost:8080");
+  tab =  await res.json();
+
+
+
+  window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+      width: '1%',
+      height: '1',
+      uri: "spotify:track:"+tab[0].id
+    };
+    const callback = (EmbedController) => {
+      document.querySelectorAll('.episode').forEach(
+        episode => {
+          episode.addEventListener('click', () => {
+            episode.setAttribute("data-spotify-id","spotify:track:"+tab[i]);
+            i = (i+1)%tab.length;
+            EmbedController.loadUri(episode.dataset.spotifyId);  
+            t_restant = 29; 
+            $('#progress').attr("style","width: " +0).attr('aria-valuenow', 0);
+            EmbedController.togglePlay();
+            $("#next").prop('disabled',true);
+            $("#pause").prop('disabled',false);
+  
+            pause=false;
+            f=setInterval(interval,1000);
+          });
+        });
+        document.querySelectorAll('.pause').forEach(
+        episode => {
+          episode.addEventListener('click', () => {
+            EmbedController.togglePlay();
+            pause = !pause;
+            if(pause == true)
+            {
+              $("#progress").removeClass("progress-bar-animated");
+            }
+            else
+            {
+              $("#progress").addClass("progress-bar-animated");
+            }
+          });
+        })
+      };
+    IFrameAPI.createController(element, options, callback);
+  
+  
+  };
+});
+
+function interval()
+{
+  if(!pause)
+  {
+    t_restant --;
+    percent = (30-t_restant)/30*100 + "%";
+    $('#progress').attr("style","width: " +percent).attr('aria-valuenow', percent).html("<b>"+t_restant+"</b>");
+    if(t_restant == 0)
+    {
+      clearInterval(f);
+      $('#progess').html("");
+      document.getElementById("pause").click();
+      $("#pause").prop('disabled',true);
+      $("#next").prop('disabled',false);
+      reveal();
+    }
+  }
+}
+
+
+function reveal()
+{
+  let img = '<img src="'+tab[i-1].img+'" alt="album">'
+  let nom = "<h5><b>"+tab[i-1].name+"</b></h5>"
+  let html = img + nom + "<p>";
+  tab[i-1].artist.forEach(element =>{
+    html = html + element;
+
+  });
+  html = html + "</p>";
+  $(".answer").html(html);
+}
